e293e9cb98a05a56ee853ce34cf434fd45d911e3
Chamath Viduranga <chamath.viduranga@airudi.com>
2022-11-10 19:20:49 +0530

    first commmit

---
+++ EmailAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using EmailService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEmailSender emailSender)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetEmail")]
+        public IEnumerable<WeatherForecast> GetEmail()
+        {
+            var rng = new Random();
+            var message = new Message(new string[] { "chamathran@gmail.com" }, "Test email", "This is the content from our email.");
+
+            _emailSender.SendEmail(message);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
